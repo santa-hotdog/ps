c2d9e7964717a696ee80ebc9be8f0ce6d181c161
박영하 (Youngha Park) <santa.hotdog@outlook.com>
2024-09-23 02:03:16 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,26 @@
+#include <bits//stdc++.h>
+using namespace std;
+
+vector<int> a;;
+vector<int> dp;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    a.resize(n);
+    dp.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    dp[0] = a[0];
+    for (int i = 1; i < n; i++) {
+        dp[i] = max(dp[i - 1] + a[i], a[i]);
+    }
+    cout << *max_element(dp.begin(), dp.end());
+}
