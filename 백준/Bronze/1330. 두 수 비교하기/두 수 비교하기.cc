0594a9ea829936b7f2786cbdaf6f629abbc0c048
박영하 (Youngha Park) <santa.hotdog@outlook.com>
2024-09-23 01:25:16 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,14 @@
+#include <bits//stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b) cout << ">";
+    else if (a < b) cout << "<";
+    else cout << "==";
+}
