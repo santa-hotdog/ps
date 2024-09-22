17caa7e423e82d0bf463a2b65a8418e73f18d251
박영하 (Youngha Park) <santa.hotdog@outlook.com>
2024-09-23 01:43:37 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -18,7 +18,7 @@ int w(int a, int b, int c) {
 int main() {
     while (1) {
         int a, b, c;
-        cin >> a >> b >> c;
+        scanf("%d %d %d", &a, &b, &c);
 
         if (a == -1 && b == -1 && c == -1) break;
         printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
